2265dceaa921e2fc5c6d88ed5ebe8286eceb82fd
Rynk-Dev <rynkdev@gmail.com>
2023-01-27 19:09:04 -0800

    Added body and wall collisions

--- src/snakegame/GamePanel.java
+++ src/snakegame/GamePanel.java
@@ -88,17 +88,43 @@ public class GamePanel extends JPanel implements ActionListener{
             case 'D':
                 y[0] = y[0] + UNIT_SIZE;
                 break;
-               case 'L':
+            case 'L':
                 x[0] = x[0] - UNIT_SIZE;
+                break;
             case 'R':
                 x[0] = x[0] + UNIT_SIZE;
+                break;
         }
     }
     public void checkApple(){
         
     }
     public void checkCollisions(){
-        
+        //check for head - body collision
+        for (int i = bodyParts; i > 0; i--){
+            if ((x[0] == x[i]) && (y[0] == y[i])){
+                running = false;
+            }
+        }
+        //check for head - left border collision
+        if (x[0] < 0){
+            running = false;
+        }
+        //check for head - right border collision
+        if (x[0] > SCREEN_WIDTH){
+            running = false;
+        }
+        //check for head - top border collision
+        if (y[0] < 0){
+            running = false;
+        }
+        //check for head - bottom border collision
+        if (y[0] > SCREEN_HEIGHT){
+            running = false;
+        }
+        if (!running) {
+            timer.stop();
+        }
     }
     public void gameOver(Graphics g){
     
@@ -117,7 +143,28 @@ public class GamePanel extends JPanel implements ActionListener{
     public class MyKeyAdapter extends KeyAdapter{
         @Override
         public void keyPressed(KeyEvent e){
-            
+            switch(e.getKeyCode()){
+                case KeyEvent.VK_LEFT:
+                    if (direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if (direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if (direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if (direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+            }// exit switch
         }
     }
 }
