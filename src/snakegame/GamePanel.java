8befcf29d54ccde266f1a02cd7659150a9f5dcce
Rynk-Dev <rynkdev@gmail.com>
2023-01-27 18:50:56 -0800

    Implemented snake graphics

--- src/snakegame/GamePanel.java
+++ src/snakegame/GamePanel.java
@@ -59,6 +59,17 @@ public class GamePanel extends JPanel implements ActionListener{
         }
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+        
+        for (int i = 0; i < bodyParts; i++){
+            if (i == 0){
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+            else {
+                g.setColor(new Color(45,180,0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
     }
     public void newApple(){
         appleX = random.nextInt((int)(SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
@@ -68,6 +79,19 @@ public class GamePanel extends JPanel implements ActionListener{
         for (int i = bodyParts; i>0; i--){
             x[i] = x[i-1];
             y[i] = y[i-1];
+        }
+        
+        switch(direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+               case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
         }
     }
     public void checkApple(){
